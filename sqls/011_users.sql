756b8254b62703d4bf4b2ac08e91ae1a63dc0a2f
siarheiNaralenkau <naralenkov2010@gmail.com>
2014-10-27 08:52:30 +0200

    Tables users and bonus_scores were added

--- sqls/011_users.sql
+++ sqls/011_users.sql
@@ -1,12 +1,7 @@
-CREATE TABLE `users` (
-	`id` INT(11) NOT NULL AUTO_INCREMENT,
-	`venue_id` INT(11) NOT NULL DEFAULT '0',
-	`username` VARCHAR(50) NOT NULL,
-	`password` VARCHAR(20) NOT NULL,
-	`bonus_scores` INT(11) NOT NULL DEFAULT '0',
-	PRIMARY KEY (`id`),
-	INDEX `fk_u_venue` (`venue_id`),
-	CONSTRAINT `fk_u_venue` FOREIGN KEY (`venue_id`) REFERENCES `venues` (`id`) ON UPDATE CASCADE ON DELETE CASCADE
+CREATE TABLE `booking_status` (
+	`id` TINYINT(4) NOT NULL,
+	`status` VARCHAR(10) NOT NULL,
+	PRIMARY KEY (`id`)
 )
 COLLATE='utf8_general_ci'
 ENGINE=InnoDB;
