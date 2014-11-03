fc91228a4a34280259c8129a59c91d7f46df399e
siarheiNaralenkau <naralenkov2010@gmail.com>
2014-11-03 08:01:34 +0300

    Booking Spend movey functionality added

---
+++ BookingServer2/src/com/dao/BookingsDAO.java
@@ -0,0 +1,63 @@
+package com.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+public class BookingsDAO {
+	private static DataSource dataSource;
+	
+	private static final String SET_BOOKING_SPENT_SQL = "UPDATE bookings SET status=6, spent_money = ? WHERE id = ?";
+	
+	static {		
+		try {
+			Context initContext = new InitialContext();
+			Context envContext  = (Context)initContext.lookup("java:/comp/env");
+			dataSource = (DataSource)envContext.lookup("jdbc/bronimesto");
+		} catch (NamingException e) {
+			e.printStackTrace();
+		}				
+	}		
+	
+	private static void closeConnection(Connection con, PreparedStatement ps) {
+		try {
+			if(ps != null) { 
+				ps.close();
+			}
+			if (con != null) {
+				con.close();
+			}
+		} catch (SQLException e) {
+			System.out.println("Unable to close connection!");
+			System.out.println(e.getMessage());
+		}			
+	}
+	
+	public static Map<String, Object> setBookingSpent(int spentMoney, int bookingId) {
+		Map<String, Object> result = new HashMap<String, Object>();
+		Connection con = null;
+		PreparedStatement ps = null;
+		try {
+			con = dataSource.getConnection();
+			ps = con.prepareStatement(SET_BOOKING_SPENT_SQL);
+			ps.setInt(1, spentMoney);
+			ps.setInt(2, bookingId);
+			ps.executeUpdate();
+			result.put("status", "success");
+		} catch(SQLException e) {
+			result.put("status", "failure");
+			result.put("error", e.getMessage());
+			e.printStackTrace();
+		} finally {
+			closeConnection(con, ps);
+		}
+		return result;
+	}
+}
---
+++ BookingServer2/src/com/servlets/SetBookingSpendServlet.java
@@ -0,0 +1,42 @@
+package com.servlets;
+
+import java.io.IOException;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.dao.BookingsDAO;
+import com.google.gson.Gson;
+
+/**
+ * Servlet implementation class SetBookingSpendServlet
+ * http://bronimesto.by:8080/BookingServer2/set_booking_spend?bookingId=31&spentMoney=350000
+ */
+@WebServlet("/set_booking_spend")
+public class SetBookingSpendServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	private static final String SPENT_MONEY = "spentMoney";
+	private static final String BOOKING_ID = "bookingId";
+    
+    public SetBookingSpendServlet() {
+        super();    
+    }
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("application/json; charset=UTF-8");
+		int spentMoney = Integer.valueOf(request.getParameter(SPENT_MONEY));
+		int bookingId = Integer.valueOf(request.getParameter(BOOKING_ID));
+		
+		Map<String, Object> result = BookingsDAO.setBookingSpent(spentMoney, bookingId);
+		
+		Gson gson = new Gson();
+		String jsonResult = gson.toJson(result);	
+		response.getWriter().write(jsonResult);
+	}
+
+}
